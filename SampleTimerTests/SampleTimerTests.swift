fda85bf337c8ca3cf1aeea11955c4f21413891d0
YugoHasegawa <15hasegawa@gmai.com>
2024-11-30 23:33:55 +0900

    Initial Commit

---
+++ SampleTimerTests/SampleTimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SampleTimerTests.swift
+//  SampleTimerTests
+//
+//  Created by 長谷川 雄吾 on 2024/11/30.
+//
+
+import Testing
+@testable import SampleTimer
+
+struct SampleTimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
